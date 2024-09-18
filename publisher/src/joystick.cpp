7e99616d8380a5572749f65c3a0e24460c634f8c
Gutnar Leede <gutnar@gmail.com>
2024-09-18 13:44:49 +0300

    Read joystick and publish messages on separate threads

--- publisher/src/joystick.cpp
+++ publisher/src/joystick.cpp
@@ -1,6 +1,9 @@
 #include <signal.h>
+#include <thread>
+#include <mutex>
 #include <iostream>
 #include <string>
+#include <cmath>
 #include <joystick.hh>
 #include <popl.hpp>
 #include <unistd.h>
@@ -26,11 +29,12 @@ int main(int argc, char *argv[])
     popl::OptionParser op("Allowed options");
     auto help = op.add<popl::Switch>("h", "help", "produce help message");
     auto device_index = op.add<popl::Value<unsigned int>>("d", "device", "device index", 0);
-    auto move_axis = op.add<popl::Value<unsigned int>>("", "move-axis", "move axis", 4);
-    auto turn_axis = op.add<popl::Value<unsigned int>>("", "turn-axis", "turn axis", 0);
+    auto move_x_axis = op.add<popl::Value<unsigned int>>("", "move-x-axis", "movement x-axis number", 3);
+    auto move_y_axis = op.add<popl::Value<unsigned int>>("", "move-y-axis", "movement y-axis number", 4);
+    auto turn_axis = op.add<popl::Value<unsigned int>>("", "turn-axis", "turn axis number", 0);
     auto max_move_speed = op.add<popl::Value<float>>("", "move-speed", "max moving speed (m/s)", 0.5);
     auto max_turn_speed = op.add<popl::Value<float>>("", "turn-speed", "max turning speed (rad/s)", 0.5);
-    auto key = op.add<popl::Value<std::string>>("", "key", "zenoh publisher key", "remote-control/{device index}");
+    auto key = op.add<popl::Value<std::string>>("", "key", "zenoh key", "rc/{device index}");
 
     try
     {
@@ -55,14 +59,14 @@ int main(int argc, char *argv[])
 
     if (!joystick.isFound())
     {
-        std::cerr << "No joystick found." << std::endl;
+        std::cerr << "Joystick not found" << std::endl;
         return EXIT_FAILURE;
     }
 
     // Auto-generate publisher key if not set
     if (!key->is_set())
     {
-        key->set_value("remote-control/" + std::to_string(device_index->value()));
+        key->set_value("rc/" + std::to_string(device_index->value()));
     }
 
     // Start zenoh session
@@ -74,60 +78,77 @@ int main(int argc, char *argv[])
     printf("Publisher key: %s\n", key->value().c_str());
     printf("Press Ctrl+C to exit\n");
 
-    // Publish speeds
-    float move_speed = 0.0;
+    // Use mutex to protect shared data
+    std::mutex mtx;
+    float move_x_speed = 0.0;
+    float move_y_speed = 0.0;
     float turn_speed = 0.0;
+
+    // Read joystick events on separate thread
+    std::thread joystick_thread([&]()
+                                {
+        while (!interrupted)
+        {
+            usleep(1000); // 1ms
+
+            // Attempt to sample an event from the joystick
+            JoystickEvent event;
+
+            if (!joystick.sample(&event))
+            {
+                continue;
+            }
+
+            mtx.lock();
+
+            // Button presses
+            if (event.isButton() && event.value == 1)
+            {
+                printf("Button %u pressed\n", event.number);
+            }
+
+            // Check move x-axis
+            if (event.isAxis() && event.number == move_x_axis->value())
+            {
+                move_x_speed = max_move_speed->value() * event.value / 32767.0;
+            }
+
+            // Check move y-axis
+            if (event.isAxis() && event.number == move_y_axis->value())
+            {
+                move_y_speed = max_move_speed->value() * -event.value / 32767.0;
+            }
+
+            // Check turn axis
+            if (event.isAxis() && event.number == turn_axis->value())
+            {
+                turn_speed = max_turn_speed->value() * -event.value / 32767.0;
+            }
+
+            mtx.unlock();
+        } });
+
+    // Publish speeds on main thread
+    std::vector<uint8_t> speeds_message(12);
 
     while (!interrupted)
     {
-        // Restrict rate
-        usleep(1000);
+        usleep(10000); // 10ms
 
-        // Attempt to sample an event from the joystick
-        JoystickEvent event;
-        bool publish = false;
+        mtx.lock();
+        memcpy(speeds_message.data(), &move_x_speed, 4);
+        memcpy(speeds_message.data() + 4, &move_y_speed, 4);
+        memcpy(speeds_message.data() + 8, &turn_speed, 4);
+        mtx.unlock();
 
-        if (!joystick.sample(&event))
-        {
-            continue;
-        }
-
-        // Button presses
-        if (event.isButton() && event.value == 1)
-        {
-            printf("Button %u down\n", event.number);
-        }
-
-        // Check move axis
-        if (event.isAxis() && event.number == move_axis->value())
-        {
-            move_speed = max_move_speed->value() * -event.value / 32767.0;
-            publish = true;
-            printf("Move speed: %f\n", move_speed);
-        }
-
-        // Check turn axis
-        if (event.isAxis() && event.number == turn_axis->value())
-        {
-            turn_speed = max_turn_speed->value() * event.value / 32767.0;
-            publish = true;
-            printf("Turn speed: %f\n", turn_speed);
-        }
-
-        // Publish speeds
-        if (publish)
-        {
-            printf("move_speed: %f, turn_speed: %f\n", move_speed, turn_speed);
-            // Serialize speeds as bytes
-            std::vector<uint8_t> speeds(8);
-            memcpy(speeds.data(), &move_speed, sizeof(float));
-            memcpy(speeds.data() + 4, &turn_speed, sizeof(float));
-            zenoh_publisher.put(speeds);
-        }
+        zenoh_publisher.put(speeds_message);
     }
 
-    // Send 0 speeds on exit
-    std::vector<uint8_t> speeds(8);
+    // Wait for joystick thread to finish
+    joystick_thread.join();
+
+    // Publish 0 speeds on exit
+    std::vector<uint8_t> speeds(12);
     zenoh_publisher.put(speeds);
 
     return EXIT_SUCCESS;
